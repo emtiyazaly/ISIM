f6512bd610255a0a9ca0859dbc44306387f87146
Emtiyaz Aly <emtiyazalysx@gmail.com>
2022-08-07 16:34:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,17 @@
 # This script is not officially supported by IBM Performance test. 
 # Author: Imtiaz Ali
 # This script is used to restart stuck ISIM Application
-# Assign your ISIM Messaging server name to ISIMMSGServerName on line 14
-# Assign your ISIM Application server name to ISIMAPPServerName on line 13
+# Assign your ISIM Messaging server name to veriable <ISIMMSGServerName>
+# Assign your ISIM Application server name to veriable <ISIMAPPServerName>
+# Assign your WebSphere Profile name to veriable <Profile_Name>
 ##############################################################################
 clear && printf '\e[3J'
 myTimeStamp=$(date +"%Y-%m-%d %H-%M-%S")
 ClientName=$(who | awk '{print $1}')
 ClientIP=$(who | awk '{print $5}')
-ISIMMSGServerName="ITIM-MSG-PRI"
-ISIMAPPServerName="ITIM-APP-PRI"
+ISIMMSGServerName="<ITIM-MSG-PRI>"
+ISIMAPPServerName="<ITIM-APP-PRI>"
+ProfileName="<ProfileName>"
 echo " ==================================================================== "
 echo "					Please Wait..."
 echo " ==================================================================== "
@@ -23,12 +25,12 @@ echo " ======================== Done with Starting NodeAgent ===================
 echo ""
 sleep 2
 echo " ======================== Starting Messaging Server ======================== "
-/opt/IBM/WebSphere/AppServer/profiles/<Profile_Name>/bin/startServer.sh ${ISIMMSGServerName}
+/opt/IBM/WebSphere/AppServer/profiles/${ProfileName}/bin/startServer.sh ${ISIMMSGServerName}
 echo " ======================== Done with Starting Messaging Server Call ======================== "
 sleep 2
 echo ""
 echo " ======================== Starting Application Server ======================== "
-/opt/IBM/WebSphere/AppServer/profiles/<Profile_Name>/bin/startServer.sh ${ISIMAPPServerName}
+/opt/IBM/WebSphere/AppServer/profiles/${ProfileName}/bin/startServer.sh ${ISIMAPPServerName}
 echo " ======================== Done with Starting Application Server Call ======================== "
 # Send an SMS & Log the activity to to get notified of the trigger.
 java -jar /tmp/sendsms.jar
